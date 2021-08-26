a69c2b02e4f340ded4a1dc217710ce22f9e401f6
Jesus <r.jesus9107@gmail.com>
2021-08-25 23:11:26 -0700

    usage details

--- src/components/GoPuffHeader.tsx
+++ src/components/GoPuffHeader.tsx
@@ -9,7 +9,6 @@ const GoPuffHeader = () => {
       <div className="card-body" style={{ }}>
         <h2 className="card-title">Driver Pay Calculator</h2>
         <p className="card-text">This calculator serves as a method to generate an <em>estimate</em> of a driver partner's pay.</p>
-        <p className="card-text"><small className="text-muted">Not to be used in conjunction with driver's, but as a tool for management to better understand driver pay.</small></p>
       </div>
     </div>
   );
