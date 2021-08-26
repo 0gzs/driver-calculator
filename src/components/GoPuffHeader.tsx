6a7b3d85404d29c1f208f7f6d22d8e4b3ccd81fd
Jesus <r.jesus9107@gmail.com>
2021-08-25 23:23:59 -0700

    usage details

--- src/components/GoPuffHeader.tsx
+++ src/components/GoPuffHeader.tsx
@@ -8,7 +8,7 @@ const GoPuffHeader = () => {
       </div>
       <div className="card-body" style={{ }}>
         <h2 className="card-title">Driver Pay Calculator</h2>
-        <p className="card-text" style={{ color: '#888'}}>Calculating driver-pay, made easy.</p>
+        <p className="card-text" style={{ color: '#888'}}>Calculating driver-pay made easy.</p>
       </div>
     </div>
   );
